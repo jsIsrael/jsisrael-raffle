37b944b08a9ae8b8d1e48beda619647b4374517b
shai <shai.reznik@gmail.com>
2015-05-14 19:11:59 +0300

    another deploy try

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,14 @@ mkdir $TEMP_DIRECTORY || exit 1
 cp -r $DIST_DIRECTORY/* $TEMP_DIRECTORY || exit 1
 cp .gitignore $TEMP_DIRECTORY || exit 1
 
-echo "Checking out gh-pages branch"
-git checkout -B gh-pages || exit 1
+
+echo -e "Adding token remote"
+git remote add origin_token $ORIGIN_URL_WITH_CREDENTIALS > /dev/null 2>&1
+echo -e "Fetching token remote"
+git fetch origin_token > /dev/null 2>&1
+echo -e "Checking out gh-pages"
+git checkout -b gh-pages --track origin_token/gh-pages
+echo "Done"
 
 echo "Removing old static content"
 git rm -rf . || exit 1
@@ -27,8 +33,10 @@ git config user.name "Travis-CI" || exit 1
 git config user.email "travis@jsisrael.com" || exit 1
 
 git add -A . || exit 1
-git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
-git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
+git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT after building Travis build $TRAVIS_BUILD_NUMBER" || exit 1
+
+echo -e "Pushing commit"
+git push -q origin_token gh-pages > /dev/null 2>&1
 
 echo "Cleaning up temp files"
 rm -Rf $TEMP_DIRECTORY
